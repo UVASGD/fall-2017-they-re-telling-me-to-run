18acc45870df7614b4117bb470f40a93e468ceb0
zsd4yr <zsd4yr@virginia.edu>
2017-06-27 18:46:22 -0400

    cave filler and procedural generation of objects and their positions

---
+++ TTMTR/Assets/Scripts/GiveRandomLocalTransformInRange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveRandomLocalTransformInRange : MonoBehaviour {
+
+	public Vector3 minimumPos;
+	public Vector3 maximumPos;
+	public Vector3 minimumRot;
+	public Vector3 maximumRot;
+	public Vector3 minimumScale;
+	public Vector3 maximumScale;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.transform.localScale = new Vector3 (
+			Random.Range (minimumScale.x, maximumScale.x), 
+			Random.Range (minimumScale.y, maximumScale.y), 
+			Random.Range (minimumScale.z, maximumScale.z));
+		this.gameObject.transform.localRotation = Quaternion.Euler (
+			Random.Range (minimumRot.x, maximumRot.x), 
+			Random.Range (minimumRot.y, maximumRot.y), 
+			Random.Range (minimumRot.z, maximumRot.z)); 
+		this.gameObject.transform.localPosition = new Vector3 (
+			Random.Range (minimumPos.x, maximumPos.x), 
+			Random.Range (minimumPos.y, maximumPos.y), 
+			Random.Range (minimumPos.z, maximumPos.z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TTMTR/Assets/Scripts/LoadInRandomPrefabs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadInRandomPrefabs : MonoBehaviour {
+
+	[SerializeField]
+	public List<GameObjectAndNumber> objs;
+
+	[System.Serializable]
+	public struct GameObjectAndNumber
+	{
+		[SerializeField]
+		public GameObject obj;
+		[SerializeField]
+		public int num;
+	}
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < objs.Count; i++){
+			for (int j = 0; j < objs[i].num; j++) {
+				Instantiate (objs[i].obj, this.gameObject.transform);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

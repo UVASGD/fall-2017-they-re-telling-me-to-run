431939f322aeb73e955b62d11aec53ccb63b4482
zsd4yr <zsd4yr@virginia.edu>
2017-06-29 17:56:49 -0400

    ensured position

--- TTMTR/Assets/Scripts/GiveRandomLocalTransformInRange.cs
+++ TTMTR/Assets/Scripts/GiveRandomLocalTransformInRange.cs
@@ -11,24 +11,53 @@ public class GiveRandomLocalTransformInRange : MonoBehaviour {
 	public Vector3 minimumScale;
 	public Vector3 maximumScale;
 
+	public string mustCollideWith;
+	public int maxNumberOfRepositions;
+	private int repositionAttempts;
+
 	// Use this for initialization
 	void Start () {
-		this.gameObject.transform.localScale = new Vector3 (
-			Random.Range (minimumScale.x, maximumScale.x), 
-			Random.Range (minimumScale.y, maximumScale.y), 
-			Random.Range (minimumScale.z, maximumScale.z));
-		this.gameObject.transform.localRotation = Quaternion.Euler (
-			Random.Range (minimumRot.x, maximumRot.x), 
-			Random.Range (minimumRot.y, maximumRot.y), 
-			Random.Range (minimumRot.z, maximumRot.z)); 
-		this.gameObject.transform.localPosition = new Vector3 (
-			Random.Range (minimumPos.x, maximumPos.x), 
-			Random.Range (minimumPos.y, maximumPos.y), 
-			Random.Range (minimumPos.z, maximumPos.z));
+		repositionAttempts = 0;
+		Rescale ();
+		Rerotate ();
+		Reposition ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void Rescale(){
+		this.gameObject.transform.localScale = new Vector3 (
+			Random.Range (minimumScale.x, maximumScale.x), 
+			Random.Range (minimumScale.y, maximumScale.y), 
+			Random.Range (minimumScale.z, maximumScale.z));
+	}
+
+	public void Rerotate(){
+		this.gameObject.transform.localRotation = Quaternion.Euler (
+			Random.Range (minimumRot.x, maximumRot.x), 
+			Random.Range (minimumRot.y, maximumRot.y), 
+			Random.Range (minimumRot.z, maximumRot.z)); 
+	}
+
+	public void Reposition(){
+		this.gameObject.transform.localPosition = new Vector3 (
+			Random.Range (minimumPos.x, maximumPos.x), 
+			Random.Range (minimumPos.y, maximumPos.y), 
+			Random.Range (minimumPos.z, maximumPos.z));
+		RaycastHit outinfo;
+		if (Physics.Raycast (transform.position, Vector3.down, out outinfo)) {
+			if (outinfo.collider.CompareTag (mustCollideWith)) {
+				return;
+			}
+		}
+		if (repositionAttempts < maxNumberOfRepositions) {
+			repositionAttempts++;
+			Reposition ();
+		} else {
+			Destroy (this.gameObject);
+		}
 	}
 }

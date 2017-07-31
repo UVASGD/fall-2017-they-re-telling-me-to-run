6c6c07614db6dd37c6f0e78e55b076cf70a2ac00
Zachary Danz <zsd4yr@users.noreply.github.com>
2017-07-31 18:24:14 -0400

    separate views and dynamic inventory grid

---
+++ TTMTR/Assets/GridLayoutSpacingFixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridLayoutSpacingFixer : MonoBehaviour {
+
+    public GridLayoutGroup gridLayoutGroup;
+    public RectTransform parentRectTransform;
+
+    public int cols = 0;
+    public int rows = 0;
+
+	// Use this for initialization
+	void Start () {
+        cols = (int)gridLayoutGroup.constraintCount;
+        rows = (gridLayoutGroup.gameObject.transform.childCount / cols);
+
+        gridLayoutGroup.cellSize = new Vector2(
+            parentRectTransform.rect.width / cols - gridLayoutGroup.spacing.x,
+            parentRectTransform.rect.height / rows - gridLayoutGroup.spacing.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TTMTR/Assets/Scripts/CameraChanger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.VR.VRSettings.showDeviceView = Globals.MATCH_DISPLAYS_FOR_HMD_AND_MONITOR;
+	}
+	
+}

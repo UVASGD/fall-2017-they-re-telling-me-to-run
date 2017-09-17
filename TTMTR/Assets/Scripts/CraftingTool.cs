d1f790bf98952a6a337511ca9d4cc3a630a07ca8
Bailey Reed <bar5dc@virginia.edu>
2017-09-17 17:17:53 -0400

    Added craftingtool class

---
+++ TTMTR/Assets/Scripts/CraftingTool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingTool : MonoBehaviour {
+
+    List<InventoryItem> listOfInternalItems = new List<InventoryItem>();
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("hello I'm a crafting tool!2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("here's our list of internal items:");
+        Debug.Log(listOfInternalItems[0]);
+        Debug.Log(listOfInternalItems.Count);
+	}
+
+	void OnTriggerEnter(Collider other) {
+	    Debug.Log("I've triggered something " + other.name);
+
+        InventoryItem script = other.gameObject.GetComponent(typeof(InventoryItem)) as InventoryItem;
+        GameObject go = other.gameObject;
+
+        if (script != null) {
+            if (script.craftable == true) {
+
+                listOfInternalItems.Add(script);
+                go.SetActive(false);
+                go.GetComponent<InventoryItem>().useable = false;
+
+            }
+
+        }
+
+	}
+}

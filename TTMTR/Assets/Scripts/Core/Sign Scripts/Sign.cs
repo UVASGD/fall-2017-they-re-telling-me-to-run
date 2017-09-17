6aab3817131772a7d941c19461527af683fe4632
Jared Capelle <jjc2bd@virginia.edu>
2017-09-17 16:43:59 -0400

    Sign Spawning Started, Mostly Working

--- TTMTR/Assets/Scripts/Core/Sign Scripts/Sign.cs
+++ TTMTR/Assets/Scripts/Core/Sign Scripts/Sign.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Sign : MonoBehaviour {
 
+    public List<GameObject> prefabs;
 	// Use this for initialization
 	void Start () {
-		
+        GameObject sign = GameObject.Instantiate(prefabs[Random.Range(0,prefabs.Count)], this.transform);
 	}
 	
 	// Update is called once per frame

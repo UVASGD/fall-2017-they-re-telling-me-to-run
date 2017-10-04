ce5460d77cfc8600e37e66b367460c425b8de2c8
Jared Capelle <jjc2bd@virginia.edu>
2017-10-04 19:56:31 -0400

    Created Sign Controller Currently just sets the prefabs list in each marker in its signMarkers array to match its own, can later be changed to change based on location and stuff.

---
+++ TTMTR/Assets/Scripts/Core/Sign Scripts/SignController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignController : MonoBehaviour {
+
+    // Use this for initialization
+    public Sign[] signMarkers;
+    public List<GameObject> possibleSigns;
+	void Awake () {
+        foreach(Sign sign in signMarkers)
+        {
+            sign.prefabs = possibleSigns;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

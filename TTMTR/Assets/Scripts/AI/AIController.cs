1cc43d4ae73cf2870d5e78df93cb1d87ca821c4b
ferociouself <jhe3cd@virginia.edu>
2017-10-04 20:06:33 -0400

    AI now wander between points and runs towards player if they get within a certain distance

--- TTMTR/Assets/Scripts/AI/AIController.cs
+++ TTMTR/Assets/Scripts/AI/AIController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AIController : MonoBehaviour {
-
-	public Transform goal;
-
+	
 	public UnityEngine.AI.NavMeshAgent navAgent;
+
+	public GameObject player;
+
+	public float playerVisualDistance = 10.0f;
+
+	public List<Transform> wanderPoints;
+
+	Transform curDest;
 
 	// Use this for initialization
 	void Start () {
-		
+		curDest = gameObject.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		navAgent.destination = goal.position;
+		if (Vector3.Distance(player.transform.position, gameObject.transform.position) < playerVisualDistance) {
+			curDest = player.transform;
+		} else {
+			if (Vector3.Distance(curDest.position, gameObject.transform.position) < 0.5f) {
+				curDest = wanderPoints[Random.Range(0, wanderPoints.Count)];
+			}
+		}
+		navAgent.destination = curDest.position;
 	}
 }

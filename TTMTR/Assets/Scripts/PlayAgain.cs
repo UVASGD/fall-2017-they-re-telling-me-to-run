86a94d842e0f6635ac9001dd00c69b40d03bec14
Cycerio <nhc6yy@virginia.edu>
2017-10-15 15:17:28 -0400

    Timer and lose screen script
    Created a timer script and a script that'll transition the lose screen back to the start of the game.

---
+++ TTMTR/Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour {
+
+	// Use this for initialization
+	public void buttonStartGame() {
+		// Change scene when clicked
+		SceneManager.LoadScene("Laura_Scene");
+	}
+	
+	// Update is called once per frame
+
+}

be64339a8909c02778b76b73121bb5a5ebbf5e4f
Zachary Danz <zsd4yr@virginia.edu>
2017-10-15 16:33:38 -0400

    first round audio stuff

---
+++ TTMTR/Assets/Scripts/PlayOnEmit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnEmit : MonoBehaviour {
+
+	private ParticleSystem particleSys;
+	private AudioSource audSrc;
+
+	private int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		particleSys = this.gameObject.GetComponent<ParticleSystem> ();
+		audSrc = this.gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (particleSys.particleCount > count) 
+		{
+			if(!audSrc.isPlaying) audSrc.Play ();
+			count++;
+		} 
+
+		if(!particleSys.isEmitting)
+		{
+			count = 0;
+		}
+	}
+}

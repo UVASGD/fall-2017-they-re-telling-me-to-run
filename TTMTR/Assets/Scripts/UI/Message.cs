05ea86cb97a8d70a11e043996f264d269207f87d
Laura Maimon <lcm4fe@virginia.edu>
2017-10-15 16:15:16 -0400

    Nest combinable item stuff (not done)

---
+++ TTMTR/Assets/Scripts/UI/Message.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator DisplayMessage(string message) {
+		GetComponent<UnityEngine.UI.Text> ().text = message;
+		// wait like 5 seconds
+		yield return new WaitForSeconds(3);
+		// call clear message
+		ClearMessage();
+
+	}
+
+	private void ClearMessage() {
+		GetComponent<UnityEngine.UI.Text> ().text = "";
+	}
+}

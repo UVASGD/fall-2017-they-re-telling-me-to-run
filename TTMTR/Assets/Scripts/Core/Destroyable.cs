942166b0e2af3a318ff4b1e7558dda5e24734099
Talia <tl2db@virginia.edu>
2017-11-19 15:20:54 -0500

    Script for destroyng an object

---
+++ TTMTR/Assets/Scripts/Core/Destroyable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour {
+	public GameObject destroyer;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == destroyer.name) {
+			Destroy (this.gameObject);
+		}
+	}
+}

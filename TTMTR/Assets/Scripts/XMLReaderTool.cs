4ff9098d7aed48ea896b600b3a8be0048c760cfb
Damon Cestaro <31603644+improvised1@users.noreply.github.com>
2017-11-19 16:15:12 -0500

    Basic signs reading from XML
    I've set up the code to read sign attributes from an XML file and to put them in variables, but it's not yet integrated with the main signs classes.

--- TTMTR/Assets/Scripts/XMLReaderTool.cs
+++ TTMTR/Assets/Scripts/XMLReaderTool.cs
@@ -9,6 +9,8 @@ public class XMLReaderTool {
 	static string XML_PATH = "Assets/Scripts/XML/";
 	static string levelType;
 	static string monsterName;
+    static List<string> spotSignsPrefabs;   //this and areaSigns are temp, and will likely be changed inorder to integrate with signs
+    static Dictionary<int, string> areaSigns;
 	
 	public XMLReaderTool (string level) {
 		levelType = level;
@@ -45,7 +47,11 @@ public class XMLReaderTool {
 		while (reader.Read ()) {
 			if (reader.NodeType == XmlNodeType.Element) {
 				if (reader.Name == "Recipes") {
-					ReadSigns (reader.ReadSubtree);
+					if (reader.Name == "AreaSpawns") {
+                        ReadAreaSigns(reader.ReadSubtree);
+                    } else if (reader.Name == "SpotSigns") {
+                        ReadSpotSigns(reader.ReadSubtree);
+                    }
 				} else if (reader.Name == "Signs") {
 					ReadRecipes (reader.ReadSubtree);
 				}
@@ -53,18 +59,58 @@ public class XMLReaderTool {
 		}
 	}
 
-	private static void ReadSigns(XmlReader reader) {
+    /*
+     * This method will be called from ReadMonsterFile() and will read in an <AreaSigns> section of the xml file, 
+     * which is composed of <signs>s holding <prefab>s and <count>s
+     */
+    private static void ReadAreaSigns(XmlReader reader) {
+        int count = -10;    //integers can't be set to null, so -10 is an impossible value that's a stand in for null
+        string prefab = null;
+
         while (reader.Read()) {
-            if (reader.NodeType == XmlNodeType.Element && reader.Name == "Signs") {
-                //To Do: Consult with the team that developed signs on what data needs to be imported, how to instantiate signs
+            if (reader.NodeType == XmlNodeType.Element) {
+
+                if (reader.Name == "prefab") {
+                    prefab = reader.Value;
+                } else if (reader.Name == "count") {
+                    int.TryParse(reader.Value, out count);
+                }
+
+                //if a value for both prefab and count has been found...
+                if (count != -10 && prefab != null) {
+                    areaSigns.Add(count, prefab);
+
+                    count = -10;
+                    prefab = null;
+                }
+
             }
         }
 
-        reader.Close ();
+        reader.Close();
+    }
 
-	}
+    /*
+     * This method will be called from ReadMonsterFile() and will read in an <SpotSigns> section of the xml file, 
+     * which is composed of <signs>s holding only <prefab>s
+     */
+    private static void ReadSpotSigns(XmlReader reader) {
+        while (reader.Read()) {
+            if (reader.NodeType == XmlNodeType.Element && reader.Name == "prefab") {
+                spotSignsPrefabs.Add(reader.Value);
+            }
+        }
 
-	private static void ReadRecipes(XmlReader reader) {
+        reader.Close();
+    }
+
+
+    /*
+     * Ending read signs sub-section
+     * Begining read recipes sub-section
+     */
+
+    private static void ReadRecipes(XmlReader reader) {
 		while (reader.Read ()) {
 			if (reader.NodeType == XmlNodeType.Element && reader.Name == "Recipe") {
 				CraftingTool.Recipe recipe = ReadRecipeFromXML (reader.ReadSubtree);

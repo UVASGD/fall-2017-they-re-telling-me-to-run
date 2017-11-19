d0331c91f504e11d77017067fa5060838d74861d
Jared Capelle <jjc2bd@virginia.edu>
2017-11-19 16:59:43 -0500

    I got little work, but I've got the algorithm (I think)

--- TTMTR/Assets/Scripts/Core/Sign Scripts/AreaSpawn.cs
+++ TTMTR/Assets/Scripts/Core/Sign Scripts/AreaSpawn.cs
@@ -23,14 +23,13 @@ public class AreaSpawn : MonoBehaviour {
     }
 
     IEnumerator  RealStart() {
-        print(Time.time);
         yield return new WaitForSeconds(delay);
-        print(Time.time);
         BoxCollider boundBox = gameObject.GetComponent<BoxCollider>();
         Vector3 center = boundBox.bounds.center;
         Vector3 size = boundBox.bounds.size;
         float diagonal = size.magnitude;
         int ignoreLayer = ~LayerMask.GetMask("SpawnAreaBoxes");
+        float coneRadius = 0.25f;
         foreach (SignWithCount signCount in possibleSigns)
         {
             for (int i=0; i < signCount.count; i++)
@@ -39,7 +38,7 @@ public class AreaSpawn : MonoBehaviour {
                     (Random.value - 0.5f) * size.x,
                     (Random.value - 0.5f) * size.y,
                     (Random.value - 0.5f) * size.z);
-                RaycastHit hit;
+                RaycastHit centerHit;
                 Vector3 direction = Vector3.zero; // This should be changed
                 switch (signCount.sign.location)
                 {
@@ -50,12 +49,25 @@ public class AreaSpawn : MonoBehaviour {
                         direction = Vector3.down;
                         break;
                     case Sign.Location.Wall:
-                        direction = new Vector3(Random.value - 0.5f, 0, Random.value - 0.5f);
+                        // direction = new Vector3(Random.value - 0.5f, 0, Random.value - 0.5f);
+                        direction = Vector3.forward;
                         break;
                 }
-                if (Physics.Raycast(newPos, direction, out hit, diagonal, ignoreLayer))
+                if (Physics.Raycast(newPos, direction, out centerHit, diagonal, ignoreLayer))
                 {
-                    Instantiate(signCount.sign, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal), gameObject.transform);
+                    if (signCount.sign.location == Sign.Location.Wall)
+                    {
+                        Vector3 perp = Vector3.Cross(direction, Vector3.up).normalized*coneRadius; 
+                        RaycastHit[] starHits = new RaycastHit[8];
+                        Physics.Raycast(newPos, direction, out starHits[0], diagonal, ignoreLayer);
+                        // Start at up, continue clockwise (0-U, 1-UR, 2-R, 3-DR, 4-D, 5-DL, 6-L, 7-UL)
+                        // Ryan is a genius, just add the vectors
+
+                    }
+                    else
+                    {
+                        Instantiate(signCount.sign, centerHit.point, Quaternion.FromToRotation(Vector3.up, centerHit.normal), gameObject.transform);
+                    }
                 }
             }
         }

7032233e68212102fe7170476e0e36cba1882bac
Laura Maimon <lcm4fe@virginia.edu>
2017-12-03 16:38:12 -0500

    Wendigo updates

---
+++ TTMTR/Assets/Trap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+	Collider collider;
+	public bool active = false;
+	GameObject lure;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("trap: " + active);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.name == "lure") {
+			lure = col.gameObject;
+			activateTrap ();
+		}
+	}
+
+	void activateTrap() {
+		active = true;
+	}
+
+	public void deactivateTrap() {
+		active = false;
+		Destroy (lure);
+	}
+}

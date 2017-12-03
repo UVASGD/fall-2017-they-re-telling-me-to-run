caa97527e5ba1db2646d1441aba6e7b9b42f51cb
Jared Capelle <jjc2bd@virginia.edu>
2017-12-03 16:27:47 -0500

    I'm 90% sure that it's working

--- TTMTR/Assets/Scripts/Core/Sign Scripts/AreaSpawn.cs
+++ TTMTR/Assets/Scripts/Core/Sign Scripts/AreaSpawn.cs
@@ -17,6 +17,7 @@ public class AreaSpawn : MonoBehaviour {
 
     // Use this for initialization
     public float delay;
+    public bool debugWalls;
     void Start()
     {
         StartCoroutine(RealStart());
@@ -58,17 +59,34 @@ public class AreaSpawn : MonoBehaviour {
                     if (signCount.sign.location == Sign.Location.Wall)
                     {
                         Vector3 perp = Vector3.Cross(direction, Vector3.up).normalized*coneRadius;
-                        Vector3 longways = direction.normalized * coneRadius;
+                        Vector3 longways = direction.normalized * centerHit.distance;
                         RaycastHit[] starHits = new RaycastHit[8];
                         // Numbers go clockwise from up (0)
-                        Vector3[] starVecs = { Vector3.up * coneRadius, Vector3.zero, perp, Vector3.zero,
-                            Vector3.down * coneRadius, Vector3.zero,  -perp, Vector3.zero};
+                        Vector3[] starVecs = { Vector3.up * coneRadius, (Vector3.up * coneRadius + perp)/2, perp, (Vector3.down * coneRadius + perp)/2,
+                            Vector3.down * coneRadius, (Vector3.up * coneRadius - perp)/2,  -perp, (Vector3.up * coneRadius - perp)/2};
                         for(int j = 0; j < 8; j++)
                         {
-                            Physics.Raycast(newPos, longways + starVecs[j], out starHits[j], diagonal * 2, ignoreLayer);
+                            Physics.Raycast(newPos, longways + starVecs[j], out starHits[j], centerHit.distance*1.25f, ignoreLayer);
                         }
-
-
+                        if (debugWalls) {
+                            for (int j = 0; j < 8; j++)
+                            {
+                                Instantiate(signCount.sign, starHits[j].point, Quaternion.FromToRotation(Vector3.up, starHits[j].normal), gameObject.transform);
+                            }
+                        }
+                        Vector3 averageNormal = centerHit.normal;
+                        int goodVecCount = 1;
+                        for(int j = 0; j < 8; j++)
+                        {
+                            if (centerHit.distance*0.875f <= starHits[j].distance && starHits[j].distance <= centerHit.distance * 1.125f)
+                            {
+                                averageNormal += starHits[j].normal;
+                                goodVecCount++;
+                            }
+                        }
+                        averageNormal /= goodVecCount;
+                        Sign newSign = Instantiate(signCount.sign, centerHit.point, Quaternion.LookRotation(averageNormal), gameObject.transform);
+                        newSign.gameObject.transform.Rotate(Vector3.up * -90);
                         // Ryan is a genius, just add the vectors
 
                     }
